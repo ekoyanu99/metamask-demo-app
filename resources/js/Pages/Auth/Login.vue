d9a0ada780e53a0d41d24ae4abf502ba041c9784
Eko Yanuarso Budi <36530602+ekoyanu99@users.noreply.github.com>
2022-01-19 23:56:13 +0700

    commit

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -52,6 +52,8 @@
 </template>
 
 <script>
+    import Web3 from 'web3/dist/web3.min.js'
+    import { useForm } from '@inertiajs/inertia-vue3'
     import { defineComponent } from 'vue'
     import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
     import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
@@ -101,8 +103,22 @@
                         onFinish: () => this.form.reset('password'),
                     })
             },
-            async loginWeb3(){
-                // Our Meta Mask integration goes here
+            async loginWeb3() {
+                if (! window.ethereum) {
+                    alert('MetaMask not detected. Please try again from a MetaMask enabled browser.')
+                }
+
+                const web3 = new Web3(window.ethereum);
+
+                const message = [
+                    "I have read and accept the terms and conditions (https://example.org/tos) of this app.",
+                    "Please sign me in!"
+                ].join("\n")
+
+                const address = (await web3.eth.requestAccounts())[0]
+                const signature = await web3.eth.personal.sign(message, address)
+
+                return useForm({ message, address, signature }).post('/login-web3')
             }
         }
     })
